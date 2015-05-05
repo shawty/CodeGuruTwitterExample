8e13c236a6e8be663ec8890d4cd21da8eb443823
Peter Shaw <shawty_ds@yahoo.com>
2015-05-05 12:25:36 +0100

    Initial build and check in

---
+++ CodeGuruTwitterExample/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace CodeGuruTwitterExample
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.pnlSearchBar = new System.Windows.Forms.Panel();
+      this.btnShowAll = new System.Windows.Forms.Button();
+      this.txtSearchTerm = new System.Windows.Forms.TextBox();
+      this.btnSearch = new System.Windows.Forms.Button();
+      this.lstFollowNames = new System.Windows.Forms.ListBox();
+      this.lstTweetList = new System.Windows.Forms.ListBox();
+      this.pnlSearchBar.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // pnlSearchBar
+      // 
+      this.pnlSearchBar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+      this.pnlSearchBar.Controls.Add(this.btnShowAll);
+      this.pnlSearchBar.Controls.Add(this.txtSearchTerm);
+      this.pnlSearchBar.Controls.Add(this.btnSearch);
+      this.pnlSearchBar.Dock = System.Windows.Forms.DockStyle.Top;
+      this.pnlSearchBar.Location = new System.Drawing.Point(0, 0);
+      this.pnlSearchBar.Name = "pnlSearchBar";
+      this.pnlSearchBar.Size = new System.Drawing.Size(912, 49);
+      this.pnlSearchBar.TabIndex = 0;
+      // 
+      // btnShowAll
+      // 
+      this.btnShowAll.Location = new System.Drawing.Point(825, 11);
+      this.btnShowAll.Name = "btnShowAll";
+      this.btnShowAll.Size = new System.Drawing.Size(75, 23);
+      this.btnShowAll.TabIndex = 2;
+      this.btnShowAll.Text = "Show All";
+      this.btnShowAll.UseVisualStyleBackColor = true;
+      this.btnShowAll.Click += new System.EventHandler(this.BtnShowAllClick);
+      // 
+      // txtSearchTerm
+      // 
+      this.txtSearchTerm.Location = new System.Drawing.Point(93, 14);
+      this.txtSearchTerm.Name = "txtSearchTerm";
+      this.txtSearchTerm.Size = new System.Drawing.Size(474, 20);
+      this.txtSearchTerm.TabIndex = 1;
+      // 
+      // btnSearch
+      // 
+      this.btnSearch.Location = new System.Drawing.Point(12, 12);
+      this.btnSearch.Name = "btnSearch";
+      this.btnSearch.Size = new System.Drawing.Size(75, 23);
+      this.btnSearch.TabIndex = 0;
+      this.btnSearch.Text = "Search";
+      this.btnSearch.UseVisualStyleBackColor = true;
+      this.btnSearch.Click += new System.EventHandler(this.BtnSearchClick);
+      // 
+      // lstFollowNames
+      // 
+      this.lstFollowNames.Dock = System.Windows.Forms.DockStyle.Left;
+      this.lstFollowNames.FormattingEnabled = true;
+      this.lstFollowNames.Location = new System.Drawing.Point(0, 49);
+      this.lstFollowNames.Name = "lstFollowNames";
+      this.lstFollowNames.Size = new System.Drawing.Size(188, 617);
+      this.lstFollowNames.TabIndex = 1;
+      this.lstFollowNames.SelectedIndexChanged += new System.EventHandler(this.LstFollowNamesSelectedIndexChanged);
+      // 
+      // lstTweetList
+      // 
+      this.lstTweetList.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.lstTweetList.FormattingEnabled = true;
+      this.lstTweetList.Location = new System.Drawing.Point(188, 49);
+      this.lstTweetList.Name = "lstTweetList";
+      this.lstTweetList.Size = new System.Drawing.Size(724, 617);
+      this.lstTweetList.TabIndex = 2;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(912, 666);
+      this.Controls.Add(this.lstTweetList);
+      this.Controls.Add(this.lstFollowNames);
+      this.Controls.Add(this.pnlSearchBar);
+      this.Name = "Form1";
+      this.Text = "My Basic Twitter App";
+      this.pnlSearchBar.ResumeLayout(false);
+      this.pnlSearchBar.PerformLayout();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Panel pnlSearchBar;
+    private System.Windows.Forms.TextBox txtSearchTerm;
+    private System.Windows.Forms.Button btnSearch;
+    private System.Windows.Forms.ListBox lstFollowNames;
+    private System.Windows.Forms.ListBox lstTweetList;
+    private System.Windows.Forms.Button btnShowAll;
+  }
+}
+
---
+++ CodeGuruTwitterExample/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using LinqToTwitter;
+
+namespace CodeGuruTwitterExample
+{
+  public partial class Form1 : Form
+  {
+    private const string MY_TWITTER_NAME = "shawty_ds";
+
+    private readonly SingleUserAuthorizer _authorizer = new SingleUserAuthorizer
+    {
+      CredentialStore = new SingleUserInMemoryCredentialStore
+      {
+        ConsumerKey = "<replace this with your consumer key>",
+        ConsumerSecret = "<replace this with your consumer secret>",
+        AccessToken = "<replace this with your access token>",
+        AccessTokenSecret = "<replace this with your access token secret>"
+      }
+    };
+
+    private List<Status> _currentTweets;
+
+    public Form1()
+    {
+      InitializeComponent();
+
+      GetMostRecent200HomeTimeLine();
+      lstTweetList.Items.Clear();
+      _currentTweets.ForEach(tweet => lstTweetList.Items.Add(tweet.User.Name + ":" + tweet.Text));
+
+      GetSideBarList(GetFollowers()).ForEach(name => lstFollowNames.Items.Add(name));
+    }
+
+    private void GetMostRecent200HomeTimeLine()
+    {
+      var twitterContext = new TwitterContext(_authorizer);
+
+      var tweets = from tweet in twitterContext.Status
+                   where tweet.Type == StatusType.Home &&
+                   tweet.Count == 200
+                   select tweet;
+
+      // NOTE: there is no error handling here, if you get an exception check the inner exception details, if your keys or auth fail this is where you'll see the fail first.
+      _currentTweets = tweets.ToList();
+    }
+
+    private List<string> GetFollowers()
+    {
+      List<string> results = new List<string>();
+
+      var twitterContext = new TwitterContext(_authorizer);
+
+      var temp = Enumerable.FirstOrDefault(from friend in twitterContext.Friendship
+                                           where friend.Type == FriendshipType.FollowersList &&
+                                                 friend.ScreenName == MY_TWITTER_NAME &&
+                                                 friend.Count == 200
+                                           select friend);
+
+      if (temp != null)
+      {
+        temp.Users.ToList().ForEach(user => results.Add(user.Name));
+
+        while (temp != null && temp.CursorMovement.Next != 0)
+        {
+          temp = Enumerable.FirstOrDefault(from friend in twitterContext.Friendship
+                                           where friend.Type == FriendshipType.FollowersList &&
+                                                 friend.ScreenName == MY_TWITTER_NAME &&
+                                                 friend.Count == 200 &&
+                                                 friend.Cursor == temp.CursorMovement.Next
+                                           select friend);
+
+          if (temp != null) temp.Users.ToList().ForEach(user => results.Add(user.Name));
+        }
+      }
+
+      return results;
+    }
+
+    private List<string> GetSideBarList(List<string> inputNames)
+    {
+      List<string> results = new List<string>();
+
+      foreach (string name in inputNames)
+      {
+        int tweetCount = _currentTweets.Count(tweet => tweet.User.Name == name);
+        results.Add(tweetCount > 0 ? string.Format("{0} ({1})", name, tweetCount) : string.Format("{0}", name));
+      }
+
+      return results;
+    }
+
+    private void LstFollowNamesSelectedIndexChanged(object sender, EventArgs e)
+    {
+      lstTweetList.Items.Clear();
+      var listBox = sender as ListBox;
+      if (listBox != null)
+      {
+        var selectedName = listBox.SelectedItem.ToString();
+        const string pattern = @"^(.*)\s\(\d{0,4}\)$";
+
+        Match match = Regex.Match(selectedName, pattern);
+
+        if (match.Success)
+        {
+          // We have a name with a count appended
+          selectedName = match.Groups[1].Value.Trim();
+        }
+
+        foreach (var tweet in _currentTweets.Where(tweet => tweet.User.Name == selectedName))
+        {
+          lstTweetList.Items.Add(tweet.User.Name + ":" + tweet.Text);
+        }
+      }
+    }
+
+    private void BtnShowAllClick(object sender, EventArgs e)
+    {
+      lstTweetList.Items.Clear();
+      _currentTweets.ForEach(tweet => lstTweetList.Items.Add(tweet.User.Name + ":" + tweet.Text));
+    }
+
+    private List<Status> SearchTwitter(string searchTerm)
+    {
+      var twitterContext = new TwitterContext(_authorizer);
+
+      var srch =
+        Enumerable.SingleOrDefault((from search in twitterContext.Search
+                                    where search.Type == SearchType.Search &&
+                                          search.Query == searchTerm &&
+                                          search.Count == 200
+                                    select search));
+      if(srch != null && srch.Statuses.Count > 0)
+      {
+        return srch.Statuses.ToList();
+      }
+
+      return new List<Status>();
+    }
+
+    private void BtnSearchClick(object sender, EventArgs e)
+    {
+      if(string.IsNullOrEmpty(txtSearchTerm.Text))
+      {
+        MessageBox.Show("Please enter a term to search for");
+        return;
+      }
+
+      var results = SearchTwitter(txtSearchTerm.Text);
+      lstTweetList.Items.Clear();
+      results.ForEach(tweet => lstTweetList.Items.Add(tweet.User.Name + ":" + tweet.Text));
+
+    }
+
+  }
+}
